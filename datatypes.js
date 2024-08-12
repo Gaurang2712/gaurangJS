b2d395f7d1e2e35b6b6a4764ee7f544b08a1d081
Gaurang2712 <gaurangchaudhary2712@gmail.com>
2024-08-12 18:39:31 +0530

    update

--- datatypes.js
+++ datatypes.js
@@ -11,3 +11,52 @@ console.log(x)
 let s_name='Gaurang'
 console.log(typeof s_name)
 console.log(s_name)
+
+//3.boolean Data type
+
+let islogin=true
+let netison=false
+
+console.log(typeof islogin)
+console.log(typeof netison)
+
+console.log(islogin)
+console.log(netison)
+
+
+//4.undefined Data type
+
+let x1
+console.log(x1)
+
+//5.Null Data type
+
+let y1=null
+console.log(typeof y1)
+
+//6.object Datatype
+
+let student={
+    name:"gaurang",
+    age:21,
+    city:"palanpur"
+}
+
+console.log(typeof student)
+console.log(student.city)
+console.log(student.age)
+console.log(student)
+
+//7.Big int Datatype
+
+let num=9638527410963852741n
+console.log(typeof num)
+console.log(num)
+
+//8.Arry Datatype
+
+let gru=[123,'raj',1331.004,963852741n,true]
+console.log(typeof gru)
+console.log(gru)
+console.log(gru[1])
+console.log(gru[3])
