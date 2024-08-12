61279ab52f724cc4c7f915c961c278f82a2ca1e4
Gaurang2712 <gaurangchaudhary2712@gmail.com>
2024-08-12 17:57:07 +0530

    update

--- datatypes.js
+++ datatypes.js
@@ -1,8 +1,13 @@
 // Data types 
 
-//1.Number Data tyepe 
-
+//1.Number Data type 
 
 let x=25
 console.log(typeof x)
 console.log(x)
+
+//2.String Data type
+
+let s_name='Gaurang'
+console.log(typeof s_name)
+console.log(s_name)
